a6160dfb0a698961e947cdc34099913ad21ef9fe
karaxis <lukaslaschitz@gmail.com>
2023-05-12 19:47:18 +0200

    IllegalArgumentException

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
@@ -1,15 +1,22 @@
 package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
+import java.net.URL;
+
 public class Bookmark {
     
-    private String URL;
+    private String URLString;
 
-
-
-    public Bookmark(String url){
-        URL = url;
+    public Bookmark(String url) throws IllegalArgumentException{
+        try{
+            new URL(url).toURI();
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException();
+        }
+        
+        URLString = url;
     }
     public String getURL() {
-        return URL;
+        return URLString;
     }
 }
